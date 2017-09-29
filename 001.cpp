ceb5589fecb26c3d1af1a15d5011d0741ffd3010
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-09-29 19:40:39 +0300

    Update 001.cpp

--- 001.cpp
+++ 001.cpp
@@ -4,13 +4,13 @@ using namespase std;
 int main()
 {
 	for (string;getline(cin, string); )
-  { int numbers[10];
+  { int n[10];
 		istringstream stream(string);
 		bool failure = false;
 		for (int i = 0; i < 10; ++i) {
-			if (!(stream >> numbers[i])) {failure = true;break;}}
-		int max = numbers[0];
-		for (int i = 0; i < 10; ++i) {if (numbers[i]>max) {max = numbers[i];}	}
+			if (!(stream >> n[i])) {failure = true;break;}}
+		int max = n[0];
+		for (int i = 0; i < 10; ++i) {if (nu[i]>max) {max = nub[i];}	}
 		if (!failure) {cout << max;}
 		else {cout << "An error has occured while reading numbers from line" <<endl;}
 		return 0;
