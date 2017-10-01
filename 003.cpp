df6eb754b9a010baf114ac43207bd23a1d5e6313
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-01 12:58:38 +0300

    Update 003.cpp

--- 003.cpp
+++ 003.cpp
@@ -1,1 +1,62 @@
+#include "iostream"
+#include "sstream"
 
+using namespace std;
+
+int main() { int max1, max2, i,j, s[10], x[10];
+
+for (string string; getline(cin, string); ) {
+	istringstream stream(string);
+	bool failure = false;
+	for (int j = 0; j < 10; ++j) {
+		if (!(stream >> s[j])) {
+			failure = true;
+			break;}	}
+	max1 = s[1];
+
+	if (!failure) {
+		for (int j = 0; j < 10; ++j) {
+			if (s[j] > max1) {
+				max1 = s[j];
+			}
+		}
+
+		break;
+	}
+	else {
+		cout << "smb will be sleep" << endl;
+	}
+}
+
+for (string string; getline(cin, string); ) {
+	istringstream stream(string);
+	bool failure = false;
+	for (int i = 0; i < 10; ++i) {
+		if (!(stream >> x[i])) {
+			failure = true;
+			break;
+		}
+	}
+
+	max2 = x[1];
+
+	if (!failure) {
+		for (int i = 0; i < 10; ++i) {
+			if (x[i] > max2) {
+				max2 = x[i];
+			}
+		}
+
+		break;
+	}
+	else {
+		cout << "smb don't let to be glad" << endl;
+	}
+} if (i>=j){
+
+cout << max1 + max2 << endl;}
+            else {
+		cout << max2 << endl;
+	}
+return 0;
+}
