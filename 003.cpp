c5496c286f92ffbf00b618e94b6941d6a38d7ab2
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-10-02 19:52:40 +0300

    Update 003.cpp

--- 003.cpp
+++ 003.cpp
@@ -1,62 +1,95 @@
 #include "iostream"
+
 #include "sstream"
+
+
 
 using namespace std;
 
-int main() { int max1, max2, maxI, maxJ;
 
-for (string string; getline(cin, string); ) { int s[10];
-	istringstream stream(string);
-	bool failure = false;
-	for (int j = 0; j < 10; ++j) {
-		if (!(stream >> s[j])) {
-			failure = true;
-			break;}	}
-	max1 = s[1];
 
-	if (!failure) {
+int main() {
+	int max1, max2, maxI, maxJ;
+
+
+
+	for (string string; getline(cin, string); ) {
+		int s[10];int x[10];
+
+		istringstream stream(string);
+
+		bool failure = false;
+
 		for (int j = 0; j < 10; ++j) {
-			if (s[j] > max1) {
-				max1 = s[j]; maxJ=j;
+
+			if (!(stream >> s[j])) {
+
+				failure = true;
+
+				break;
 			}
 		}
 
-		break;
-	}
-	else {
-		cout << "smb will be sleep" << endl;
-	}
-}
-
-for (string string; getline(cin, string); ) {int x[10];
-	istringstream stream(string);
-	bool failure = false;
-	for (int i = 0; i < 10; ++i) {
-		if (!(stream >> x[i])) {
-			failure = true;
-			break;
-		}
-	}
-
-	max2 = x[1];
-
-	if (!failure) {
+		max1 = s[1];
 		for (int i = 0; i < 10; ++i) {
-			if (x[i] > max2) {
-				max2 = x[i];maxI=i;
+
+			if (!(stream >> x[i])) {
+
+				failure = true;
+
+				break;
+
 			}
+
 		}
 
-		break;
-	}
-	else {
-		cout << "smb don't let to be glad" << endl;
-	}
-} if (maxI>=maxJ){
+		max2 = x[1];
 
-cout << max1 + max2 << endl;}
-            else {
-		cout << max2 << endl;
+		if (!failure) {
+
+			for (int j = 0; j < 10; ++j) {
+
+				if (s[j] > max1) {
+
+					max1 = s[j]; maxJ = j;
+
+				}
+
+			}
+			for (int i = 0; i < 10; ++i) {
+
+				if (x[i] > max2) {
+
+					max2 = x[i]; maxI = i;
+
+				}
+
+			}
+
+		}
+
+		else {
+
+			cout << "smb will be sleep" << endl;
+
+		}
+
 	}
-return 0;
+	
+
+	if (maxI >= maxJ) {
+
+
+
+		cout << max1 + max2 << endl;
+	}
+
+	else {
+
+		cout << max2 << endl;
+
+	}
+
+	return 0;
+
 }
